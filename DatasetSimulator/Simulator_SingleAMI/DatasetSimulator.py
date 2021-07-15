8444d3ee523f9bfb8994a44697bd667cc03c8113
Shun Hui <56659883+Shun-97@users.noreply.github.com>
2021-07-15 13:14:27 +0800

    Revamped

---
+++ DatasetSimulator/Simulator_SingleAMI/DatasetSimulator.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import math
+
+instance_count = 0
+instance_hours = {} #how many AWS hours in total, need to initialize to 0
+available = [] #contain instanceId
+occupied = {} #contain userId, instanceId [0], time start used [1]
+
+
+def get_instance(userId, timestamp):
+	global instance_count
+	global instance_seconds
+	global instance_hours
+	global available
+	global occupied
+
+	if (len(available) == 0):
+		instance_count += 1
+		occupied[userId] = [instance_count, timestamp]
+		instance_hours[instance_count] = 0
+	else:
+		occupied[userId] = [available[0], timestamp]
+		available.pop(0)
+
+def release_instance(userId, timestamp):
+	global instance_count
+	global instance_seconds
+	global instance_hours
+	global available
+	global occupied
+
+	ins = occupied[userId][0]
+	instance_hours[ins] += math.ceil((timestamp - occupied[userId][1])/3600)
+	available.append(ins)
+	del occupied[userId]
+
+def main():
+	df_sorted = pd.read_csv("./DataSet.txt", sep=" ")
+	# print(df_sorted)
+
+	for index, row in df_sorted.iterrows():
+		# print(index, row)
+		if(row["action"] == "start"):
+			get_instance(row["id"], row["timestamp"])
+		elif(row["action"] == "end"):
+			release_instance(row["id"], row["timestamp"])
+
+	instances = list(instance_hours.keys())
+	instances.sort()
+	total = 0
+	for ins in instances:
+		total += instance_hours[ins]
+		print("Instance number: " + str(ins) + ", Hours: " + str(instance_hours[ins]))
+	print(f"Total Hours: {total}")
+
+
+	# active = []
+	# o = open("./wow_cleaned_final.txt","w")
+
+	# data = pd.read_csv("./WoWSessionData.txt", sep=", ")
+	# filtered = data.loc[data['Event'].isin(["PLAYER_LOGIN","PLAYER_LOGOUT"])]
+	# sorted_filtered = filtered.sort_values(by=['Timestamp'])
+
+	# print(sorted_filtered)
+
+	# o.write("id action timestamp\n")
+
+	# for index, row in sorted_filtered.iterrows():
+	# 	print(index, row)
+	# 	if(row["Category"] == "SESSION_START"):
+	# 		if (row["PlayerID"] in active):
+	# 			continue
+	# 		else:
+	# 			active.append(row["PlayerID"])
+	# 			o.write(str(row["PlayerID"]) + " start " + str(int(row["Timestamp"])) + "\n")
+
+	# 	elif(row["Category"] == "SESSION_END"):
+	# 		if (row["PlayerID"] not in active):
+	# 			continue
+	# 		else:
+	# 			active.remove(row["PlayerID"])
+	# 			o.write(str(row["PlayerID"]) + " end " + str(int(row["Timestamp"])) + "\n")
+
+	# for playerId in active:
+	# 	o.write(str(playerId) + " end " + str(1246657574) + "\n")
+	# 	active.remove(playerId)
+
+	# print(active)
+
+if __name__ == '__main__' :
+	main()
--- README.md
+++ README.md
@@ -1,21 +1,33 @@
 # CostSimulation
 
-# Required Packages
+## Description
+Programs used by the team to generate data for the report.
+
+### Folders Explanation
+- Dataset Used: Dataset used to simulate in our report
+- Dataset Creation:
+    - AMI_allGames: attach ami (30 in total, each containing 1 game) to each session at a weighted chance randomly
+    - AMI_SetOFGamesRandom: attach ami (6 in total, each containing 5 games) to each session at a weighted chance randomly
+    - Randomiser: Allow user to create their own dataset randomly. Note: this is less accurate compared to using the WOW_session 
+- DatasetSimulator: Run simulation based on the number of ami in total
+
+# HOW TO USE
+## Required Packages
 1. Pandas -> <code>pip install pandas</code>
 
-# General
+## General
 1. In your chosen command-line/ terminal.
 2. Navigate to the project folder
 
-## To create Dataset
+### To create Dataset
 1. type <code>python DatasetCreation\Randomiser.py</code> 
-2. type <code>python "DatasetCreation\AMI Random.py"</code> 
+2. type <code>python "DatasetCreation\AMI Random.py"</code>
 
-## To test data
-### If all instances runs a single AMI
-1. type <code>python DatasetSimulator\DatasetSimulator.py</code> 
-2. type <code>python DatasetSimulator\DatasetSimulatorOptimized.py</code> 
+### To test data
+#### If all instances runs a single AMI
+1. type <code>python DatasetSimulator\Simulator_SingleAMI\DatasetSimulator.py</code> 
+2. type <code>python DatasetSimulator\Simulator_SingleAMI\DatasetSimulatorOptimized.py</code> 
 
-### If different instance has different AMI
-1. type <code>python DatasetSimulator_amiVersion\DatasetSimulator.py</code> 
-2. type <code>python DatasetSimulator_amiVersion\DatasetSimulatorOptimized.py</code> 
+#### If different instance has multiple AMI
+1. type <code>python DatasetSimulator\Simulator_MultipleAMI\DatasetSimulator.py</code> 
+2. type <code>python DatasetSimulator\Simulator_MultipleAMI\DatasetSimulatorOptimized.py</code> 
